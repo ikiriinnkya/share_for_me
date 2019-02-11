b29384035551c24af5ae37b77ba14cff1d27aedc
ikiriinnkya <44297375+ikiriinnkya@users.noreply.github.com>
2019-02-11 23:29:10 +0900

    Add files via upload

---
+++ get_distance.py
@@ -0,0 +1,12 @@
+import math
+
+data = (1.0,1.0,0,0)
+
+def get_distance_rad(goal_x,goal_y,present_x,present_y):
+    LATI = 91141.288#north 35
+    LONG = 111319.49
+    delta_x = (goal_x - present_x)*LONG
+    delta_y = (goal_y - present_y)*LATI
+    return (delta_x**2 + delta_y**2)**0.5,math.atan2(delta_y,delta_x)
+
+print get_distance_rad(*data)
